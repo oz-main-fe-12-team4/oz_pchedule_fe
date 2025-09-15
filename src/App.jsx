e71a701ec86ae6c5ed1280af2ba2d1fb9bcb98bf
danbi-seo <sdb0504@naver.com>
2025-09-15 15:50:44 +0900

    feat: 공통 Button 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Button } from "./assets/components/Button";
+
 function App() {
   return (
     <>
-      <div>pchedule</div>
+      <Button
+        onCancelClick={() => console.log("취소 클릭")}
+        onConfirmClick={() => console.log("저장 클릭")}
+      />
     </>
   );
 }
