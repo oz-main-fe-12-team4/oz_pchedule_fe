b5dda2bdf3d8cadc1800f97e4a3530e0226e7623
jongwon-mac <jaketd15@gmail.com>
2025-09-17 02:20:01 -0700

    feat: 확인창 UI구현

--- src/components/ConfirmModal.jsx
+++ src/components/ConfirmModal.jsx
@@ -7,8 +7,19 @@ function ConfirmModal({
 }) {
   return (
     <div className="fixed inset-0 bg-gray-300 bg-opacity-40 flex items-center justify-center z-50">
-      <div className="bg-gray-700 rounded-lg p-6 w-[320px] shadow-lg flex flex-col items-center justify-center text-center">
-        <p className="mb-6 text-white">{message}</p>
+      <div
+        className="relative bg-gray-700 rounded-lg p-6 w-[320px] shadow-lg 
+      flex flex-col items-center justify-center text-center"
+      >
+        {/* 오른쪽 상단에 둥근 닫기 버튼 */}
+        <div
+          className="absolute top-1 right-1 w-4 h-4 rounded-full
+         bg-gray-500 text-white flex items-center justify-center cursor-pointer select-none"
+        >
+          ×
+        </div>
+
+        <p className="mb-3 text-white">{message}</p>
         <div className="flex justify-center space-x-4">
           <button
             className="bg-black text-white rounded px-4 py-2 hover:bg-gray-900 "
