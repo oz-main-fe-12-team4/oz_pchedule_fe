7d3526b290bbddae93a144d6b4981f9cada87875
jongwon-mac <jaketd15@gmail.com>
2025-09-16 18:36:40 -0700

    docs: 스토리카드 수정

---
+++ src/assets/data/dummySchedule.js
@@ -0,0 +1,11 @@
+export const dummySchedule = {
+  post_id: 1,
+  title: "일정스토리의 제목입니다.",
+  start_time: "...",
+  category: "...",
+  is_shared: true,
+  like_count: 500,
+  favorite_count: 200,
+  is_reported: false,
+  is_completed: false,
+};
--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,23 +1,5 @@
-import ScheduleStoryCard from "./ScheduleStoryCard";
-
 function Test() {
-  const dummySchedule = {
-    post_id: 1,
-    title: "일정스토리의 제목입니다.",
-    start_time: "...",
-    category: "...",
-    is_shared: true,
-    like_count: 500,
-    favorite_count: 200,
-    is_reported: false,
-    is_completed: false,
-  };
-
-  return (
-    <div>
-      <ScheduleStoryCard schedule={dummySchedule} />
-    </div>
-  );
+  return <div>test</div>;
 }
 
 export default Test;
