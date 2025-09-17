4d7f71da9c34ef1dae63658e0e4e7dad9ab7019c
Neungjimus <lingchihhao@gmail.com>
2025-09-17 14:36:09 +0900

    [feat]북마크버튼활성화

--- src/components/BookmarkButton.jsx
+++ src/components/BookmarkButton.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { FaBookmark, FaRegBookmark } from "react-icons/fa";
 
 export default function BookmarkButton() {
-  const isBookmarked = true; // UI 테스트를 위해 '찜 완료' 상태로 고정
+  const [isBookmarked, setIsBookmarked] = useState(false); // 초기 상태를 false로 설정
+
+  const handleClick = () => {
+    // 클릭할 때마다 상태를 반전시킵니다.
+    setIsBookmarked(!isBookmarked);
+  };
+
   const buttonText = isBookmarked ? "찜 완료" : "찜하기";
 
   return (
     <button
       className="bg-transparent border-none p-1.5 inline-flex items-center justify-center space-x-1 cursor-pointer"
       title={buttonText}
+      onClick={handleClick} // 클릭 이벤트 핸들러를 연결합니다.
     >
       {isBookmarked ? (
         <FaBookmark className="text-amber-500" />
