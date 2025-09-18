f9ec7b659b74eda4319a1413b2039977bcd5670e
rimi-w <whi1230@naver.com>
2025-09-18 13:29:19 +0900

    fix : input props 변경 및 Button 컴포넌트 변경(#25)

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Input from "../components/Input";
 import { Link } from "react-router";
 import logo from "../assets/Logo.svg";
+import { Button } from "../components/Button";
 
 function Signin() {
   const [nameInputValue, setNameInputValue] = useState("");
@@ -17,49 +18,51 @@ function Signin() {
       >
         <img src={logo} alt="pchedule logo" className="w-[300px] scale-70" />
         <div className="flex flex-col gap-1 justify-center items-start">
-          <h2 className="text-[13px]">이름</h2>
           <Input
             type={"text"}
             placeHolder={"이름을 입력하세요."}
+            label={"이름"}
+            inputId={"이름"}
             value={nameInputValue}
             setValue={setNameInputValue}
             required={true}
           />
         </div>
         <div className="flex flex-col gap-1 justify-center items-start">
-          <h2 className="text-[13px]">이메일</h2>
           <Input
             type={"email"}
             placeHolder={"이메일을 입력하세요."}
+            label={"이메일"}
+            inputId={"이메일"}
             value={emailInputValue}
             setValue={setEmailInputValue}
             required={true}
           />
         </div>
         <div className="flex flex-col gap-1 justify-center items-start">
-          <h2 className="text-[13px]">비밀번호</h2>
           <Input
             type={"password"}
             placeHolder={"비밀번호를 입력하세요."}
+            label={"비밀번호"}
+            inputId={"비밀번호"}
             value={passwordInputValue}
             setValue={setPasswordInputValue}
             required={true}
           />
         </div>
         <div className="flex flex-col gap-1 justify-center items-start">
-          <h2 className="text-[13px]">비밀번호 확인</h2>
           <Input
             type={"password"}
             placeHolder={"비밀번호를 한번 더 입력하세요."}
+            label={"비밀번호 확인"}
+            inputId={"비밀번호확인"}
             value={checkPasswordInputValue}
             setValue={setCheckPasswordInputValue}
             required={true}
             compareValue={passwordInputValue}
           />
         </div>
-        <button className="w-[300px] h-8 m-3 rounded-xl bg-[#D9D9D9] hover:bg-[#5AA5B2]">
-          회원가입
-        </button>
+        <Button variant={"login"} children={"회원가입"} />
       </form>
       <p className="w-[300px] text-center p-8 border-t border-[#d9d9d9]">
         계정이 있으신가요?{" "}
