51b01ea9f86d253c9460f37fa22cb0805327951f
rimi-w <whi1230@naver.com>
2025-09-19 00:04:11 +0900

    feat: 오늘 버튼 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,21 +2,25 @@ import { useState } from "react";
 import MainCalendar from "../components/calendar/MainCalendar";
 import { MdOutlineNavigateNext } from "react-icons/md";
 import { MdOutlineNavigateBefore } from "react-icons/md";
+import { Button } from "../components/Button";
 
-function Main() {
+const Main = () => {
   const [currentDate] = useState(new Date());
   const thisYear = currentDate.getFullYear();
   const thisMonth = currentDate.getMonth() + 1;
 
   return (
     <div className="h-[calc(100vh-100.18px)]">
-      <div className="flex items-center gap-5 p-[28px_0_18px_28px] text-2xl font-extrabold">
-        {thisYear}.{thisMonth}
-        <MdOutlineNavigateBefore />
-        <MdOutlineNavigateNext />
+      <div className="flex items-center justify-between px-8">
+        <div className="flex items-center gap-5 p-[28px_0_18px_0] text-2xl font-extrabold">
+          {thisYear}.{thisMonth}
+          <MdOutlineNavigateBefore />
+          <MdOutlineNavigateNext />
+        </div>
+        <Button children={"오늘"} />
       </div>
       <MainCalendar year={thisYear} month={thisMonth} />
     </div>
   );
-}
+};
 export default Main;
