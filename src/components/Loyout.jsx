62e21a7369116d7657d7b4ffd5e161986b02bb9a
rimi-w <whi1230@naver.com>
2025-09-18 19:04:32 +0900

    feat: MenuBar 생성 및 layout 배치

--- src/components/Loyout.jsx
+++ src/components/Loyout.jsx
@@ -1,3 +1,15 @@
+import { Outlet } from "react-router";
+import { Header } from "./Header";
+import MenuBar from "./MenuBar";
+
 export const Layout = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <div>
+        <MenuBar />
+        <Outlet />
+      </div>
+    </div>
+  );
 };
