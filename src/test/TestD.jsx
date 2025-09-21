c5af0b760f0640fc70431ae4fb9df644fbc92855
danbi-seo <sdb0504@naver.com>
2025-09-22 00:08:23 +0900

    feat : TobButton을 컴포넌트로 재사용가능하게 분리(#75)

--- src/test/TestD.jsx
+++ src/test/TestD.jsx
@@ -1,8 +1,5 @@
 import Header from "../components/Header";
 import MenuBar from "../components/MenuBar";
-import PlusButton from "../components/PlusButton";
-import DailyPage from "../pages/DailyPage";
-import { posts } from "../assets/data/dummyPostList";
 
 function TestD() {
   return (
@@ -10,9 +7,9 @@ function TestD() {
       <Header />
       <div className="flex">
         <MenuBar />
-        <DailyPage posts={posts} />
       </div>
     </div>
   );
 }
+
 export default TestD;
