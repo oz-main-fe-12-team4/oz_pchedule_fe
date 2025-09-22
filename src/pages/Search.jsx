235f638245df77a0f3ec57034037cff1c5f7bf97
rimi-w <whi1230@naver.com>
2025-09-22 11:24:04 +0900

    feat: tapButton 컴포넌트 사용:

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import Input from "../components/Input";
 import { RiFileSearchFill } from "react-icons/ri";
+import TabButton from "../components/TabButton";
 
 function Search() {
   const [searchString, setSearchString] = useState("");
-  const [activeTab, setActiveTab] = useState("전체");
   const tabs = ["전체", "할일", "루틴", "언젠가", "일정스토리"];
 
   return (
@@ -18,23 +18,7 @@ function Search() {
         />
       </div>
 
-      <div className="border-b border-gray-200">
-        <div className="flex">
-          {tabs.map((tab) => (
-            <button
-              key={tab}
-              onClick={() => setActiveTab(tab)}
-              className={`px-6 py-4 text-sm font-medium border-b-2 transition-colors ${
-                activeTab === tab
-                  ? "text-gray-900 border-gray-900 font-extrabold"
-                  : "text-gray-500 border-transparent hover:text-gray-700 hover:border-gray-300"
-              }`}
-            >
-              {tab}
-            </button>
-          ))}
-        </div>
-      </div>
+      <TabButton tabs={tabs} />
 
       <div className="flex flex-col justify-center items-center gap-5 text-[#c2c2c2] p-20">
         <RiFileSearchFill size={100} />
