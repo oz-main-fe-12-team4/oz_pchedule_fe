99df14f9eedfba284cd4c661daf39f3f677309ab
Neungjimus <lingchihhao@gmail.com>
2025-09-24 17:03:11 +0900

    fix:더미유저관련파일수정

---
+++ src/assets/data/dummyUserList.js
@@ -0,0 +1,30 @@
+export const userList = {
+  data: [
+    {
+      id: 1,
+      email: "wating@gmail.com",
+      name: "박대기",
+      is_active: true,
+      is_reported: false,
+      report_reason: "",
+    },
+    {
+      id: 2,
+      email: "jihojiho@gmail.com",
+      name: "능지호",
+      is_active: true,
+      is_reported: true,
+      report_reason: "나에게 모욕감을 줌",
+    },
+    {
+      id: 3,
+      email: "best-ing@gmail.com",
+      name: "최선중",
+      is_active: false,
+      is_reported: false,
+      report_reason: "",
+    },
+  ],
+  total_user: 10,
+  total_reported_user: 5,
+};
