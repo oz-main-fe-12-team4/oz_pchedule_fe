cdcfa0e6442a883f7366e854da8eba9f31f204ff
Neungjimus <lingchihhao@gmail.com>
2025-09-24 11:14:35 +0900

    feat:최종수정

--- src/assets/data/dummyUser.js
+++ src/assets/data/dummyUser.js
@@ -24,7 +24,7 @@ export const userList = [
     report_reason: "",
   },
 ];
-  
+
 export const dummyUser = {
   user_id: 4,
   email: "test@naver.com",
@@ -33,3 +33,6 @@ export const dummyUser = {
   is_reported: false,
   report_reason: "",
 };
+
+export const dummyLikes = 123;
+export const dummyBookmarks = 45;
