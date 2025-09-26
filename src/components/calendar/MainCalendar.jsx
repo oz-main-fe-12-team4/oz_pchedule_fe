9ec8bbc902be05aa15ff58e63255f9a216ad5be6
danbi-seo <sdb0504@naver.com>
2025-09-26 16:12:13 +0900

    chore : 기존 FirstDayOfMonth & LastDayOfMotnh로 변경(#82)

--- src/components/calendar/MainCalendar.jsx
+++ src/components/calendar/MainCalendar.jsx
@@ -4,12 +4,10 @@ import { groupDatesByWeek } from "../../utils/groupDatesByWeek";
 
 function MainCalendar({ year, month, onDayClick }) {
   // 매 달 1일
-  // const firstDayOfMonth = new Date(year, month, 1);
-  const firstDayOfMonth = new Date(year, month - 1, 1);
+  const firstDayOfMonth = new Date(year, month, 1);
 
   // 매 달의 마지막 날 (30일, 31일)
-  // const lastDayOfMonth = new Date(year, month + 1, 0);
-  const lastDayOfMonth = new Date(year, month, 0);
+  const lastDayOfMonth = new Date(year, month + 1, 0);
 
   const weeks = groupDatesByWeek(firstDayOfMonth, lastDayOfMonth); // 주들을 모을 배열
 
