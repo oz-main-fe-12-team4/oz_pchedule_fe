6964e06734cb53001acbd538d2aa786b6fca35b9
rimi-w <whi1230@naver.com>
2025-09-27 18:02:54 +0900

    feat: 유저리스트 api 연결 및 렌더링 (#122)

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import UserCard from "../components/UserCard.jsx";
 import Input from "../components/common/Input.jsx";
-import { userList } from "../assets/data/dummyUser.js";
 import Button from "../components/common/Button.jsx";
+import { useLoaderData } from "react-router";
 
 function AdminPage() {
+  const data = useLoaderData();
+  const userList = data.data;
   const [searchQuery, setSearchQuery] = useState("");
   const [isReportedOnly, setIsReportedOnly] = useState(false);
 
   // 검색어와 필터링 상태에 따라 유저 목록을 동적으로 필터링
-  const filteredUsers = userList.filter((user) => {
-    const matchesSearch =
-      user.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      user.email.toLowerCase().includes(searchQuery.toLowerCase());
-    const matchesFilter = isReportedOnly ? user.is_reported : true;
-    return matchesSearch && matchesFilter;
-  });
+  // const filteredUsers = userList.filter((user) => {
+  //   const matchesSearch =
+  //     user.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+  //     user.email.toLowerCase().includes(searchQuery.toLowerCase());
+  //   const matchesFilter = isReportedOnly ? user.is_reported : true;
+  //   return matchesSearch && matchesFilter;
+  // });
 
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
@@ -30,7 +32,8 @@ function AdminPage() {
       <div className="flex flex-col gap-2 pb-5 border-b">
         <h1 className="text-2xl font-bold mb-4">유저 목록</h1>
         <p className="text-gray-300">
-          전체 {}명 신고된 유저 {}명
+          전체 {userList.total_user}명 신고된 유저{" "}
+          {userList.total_reported_user}명
         </p>
       </div>
       <div className="flex flex-col gap-3 pb-3 border-b">
@@ -48,8 +51,8 @@ function AdminPage() {
       </div>
 
       <div className="">
-        {filteredUsers.length > 0 ? (
-          filteredUsers.map((user) => <UserCard key={user.id} user={user} />)
+        {userList.length > 0 ? (
+          userList.map((user) => <UserCard key={user.id} user={user} />)
         ) : (
           <div className="text-gray-500 text-center">검색 결과가 없습니다.</div>
         )}
